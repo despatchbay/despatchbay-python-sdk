a7bf56723266f1732e6526c104d469123283f7bc
Scott Keenan <scott.keenan@salegroup.co.uk>
2019-05-02 14:41:37 +0100

    setup chnages

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='despatchbay',
-    version='0.1',
-    description='Despatchbay SDK',
-    url='',
-    author='',
-    author_email='',
-    license='MIT',
-    packages=['despatchbay'],
-    zip_safe=False
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+print(setuptools.find_packages())
+
+setuptools.setup(
+    name="despatchbay",
+    version="0.0.5",
+    author="Despatch Bay",
+    author_email="scott.keenan@thesalegroup.co.uk",
+    description="Python SDK for the Despatch Bay API v15",
+    install_requires=['suds-py3', 'requests'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
