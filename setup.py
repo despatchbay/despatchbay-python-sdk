4355fc755429f65d085a16403637ead15e06294f
Scott Keenan <scott.keenan@salegroup.co.uk>
2019-07-04 16:59:58 +0100

    setup.py using implicit package names

--- setup.py
+++ setup.py
@@ -3,19 +3,17 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-print(setuptools.find_packages())
-
 setuptools.setup(
     name="despatchbay",
-    version="0.0.5",
+    version="0.5.3",
     author="Despatch Bay",
     author_email="scott.keenan@thesalegroup.co.uk",
     description="Python SDK for the Despatch Bay API v15",
     install_requires=['suds-py3', 'requests'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_namespace_packages(include=['despatchbay-python-sdk.despatchbay']),
+    url="https://test.pypi.org/project/despatchbay/",
+    packages=setuptools.find_namespace_packages(include=['despatchbay']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
